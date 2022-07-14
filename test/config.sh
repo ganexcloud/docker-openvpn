df5077c706805b6a56b65eb04c1a327a959121a4
Caio Henrique <caio@ganex.com.br>
2022-07-14 10:50:56 -0300

    feat: Add initial files

---
+++ test/config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+testAlias+=(
+	[ganex/openvpn]='openvpn'
+)
+
+imageTests+=(
+	[openvpn]='
+	paranoid
+        conf_options
+        client
+        basic
+        dual-proto
+        otp
+	iptables
+	revocation
+	'
+)
